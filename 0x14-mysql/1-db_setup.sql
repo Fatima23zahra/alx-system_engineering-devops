b5965d013ac37dd1ee65d52a6b65c3817ba776a8
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2024-01-06 01:34:51 -0800

    a

--- 0x14-mysql/1-db_setup.sql
+++ 0x14-mysql/1-db_setup.sql
@@ -1,4 +1,4 @@
--- Creates a user with a replication client privilege
+-- lets create a user on the database servers
 CREATE USER IF NOT EXISTS 'holberton_user'@'localhost' IDENTIFIED BY 'projectcorrection280hbtn';
-GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+GRANT REPLICATION CLIENT, REPLICATION SLAVE ON *.* TO 'holberton_user'@'localhost';
 FLUSH PRIVILEGES;
