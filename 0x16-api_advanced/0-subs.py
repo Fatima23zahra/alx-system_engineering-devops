49f5d77af086bf14b54a95d904b19fbcd5735084
Fatima23zahra <fatimaezzahra.aitchouari@gmail.com>
2024-02-25 16:27:32 -0800

    a

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,20 +1,17 @@
 #!/usr/bin/python3
-"""Define number_of_subscribers function"""
+"""
+Contains the number_of_subscribers function
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Query the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    - If an invalid subreddit is given, the function should return 0.
-    """
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x016.project:v1.0.0 (by /u/ecalvoc)"
-        }
-    subreddit_data = requests.get(url,
-                                  headers=headers,
-                                  allow_redirects=False).json().get("data")
-    if subreddit_data:
-        return subreddit_data.get("subscribers")
-    return 0
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
